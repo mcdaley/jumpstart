1055411c02654ec7806944b3c9f43d2402f4b605
Mike Daley <michaelcdaley@gmail.com>
2018-02-22 13:43:03 -0800

    Moved the JavaScript files from app/assets/javascripts to app/javascript/src/js and, so that the JavaScript files will be handled by webpacker instead of with sprockets and the asset pipeline.
    Also, added jquery so that I can use the bootstrap js components.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,7 @@
+/******************************************************************************
+ * app/javascript/packs/application.js
+ ******************************************************************************/
+
 /* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
@@ -7,4 +11,10 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+import Rails      from 'rails-ujs'
+import Turbolinks from 'turbolinks'
+
+import '../src/js/pages'
+
+Rails.start()
+Turbolinks.start()
